a77b4227c04e090201f51acd59024cb79013c069
makwanaj789-sys <makwanaj789@gmail.com>
2026-07-31 18:51:02 +0530

    Update provider.py

--- api/provider.py
+++ api/provider.py
@@ -13,7 +13,7 @@ class Provider:
 
     def __init__(self):
         self.jiosaavn = Jiosaavn()
-        self.youtube = download_video()
+        self.youtube = download_video
 
     # =====================================
     # SEARCH
