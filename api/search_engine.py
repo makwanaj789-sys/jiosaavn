0f91cc0dbd9188fafeceeb7be7cfdd6d2f128982
makwanaj789-sys <makwanaj789@gmail.com>
2026-08-07 08:36:18 +0530

    Update search_engine.py

--- api/search_engine.py
+++ api/search_engine.py
@@ -64,7 +64,7 @@ class SearchEngine:
             "remote_components": ["ejs:github"],
             "extractor_args": {
                 "youtube": {
-                    "player_client": ["web"]
+                    "player_client": ["web"],
             "postprocessors": [{
               "key": "FFmpegExtractAudio",
               "preferredcodec": "mp3",
